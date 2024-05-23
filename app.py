a3890eda434420508573822e04f4008c6eec646a
leminht <leminht@oregonstate.edu>
2024-05-23 20:41:19 +1000

    modified index.html

--- app.py
+++ app.py
@@ -1,29 +1,11 @@
-# from flask import Flask, jsonify, request, send_from_directory
-
-# app = Flask(__name__, static_folder='static')
-
-# # Dummy data endpoint
-# @app.route('/data', methods=['GET'])
-# def get_data():
-#     return jsonify({"message": "Hello, world!"})
-
-# # Route for serving the index.html file
-# @app.route('/')
-# def index():
-#     return send_from_directory(app.static_folder, 'index.html')
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
-
-from flask import Flask, jsonify, render_template_string
-import random
+from flask import Flask, send_file
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template_string(open('templates/index.html').read())
 
+    return send_file('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
